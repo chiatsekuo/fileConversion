1bc491d06cdd8acc79d52d46c7054db9b93baa25
chiatsekuo <jonathan.kuo33@gmail.com>
2020-03-27 21:58:42 +0800

    initial commit

---
+++ xlsToJson-excel2json.py
@@ -0,0 +1,13 @@
+
+import excel2json
+
+excel2json.convert_from_file('xlsToJson.xlsx')
+
+'''
+Limitations of excel2json-3 module
+
+The plugin has very limited features.
+There are no options to skip any sheet, rows, and columns. This makes it hard to use with bigger excel files.
+The JSON is saved into files. Most of the times, we want to convert to JSON and use it in our program rather than saving it as a file.
+The integers are getting converted to the floating point numbers.
+'''
